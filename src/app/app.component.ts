8f626f8a6497ac6d622a112367de71c90cea7970
Luciane Santos <lucianesantcs@gmail.com>
2021-10-10 12:14:22 -0300

    feat(avancando-acessibilidade): aula final 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { fade } from './shared/animations/fade';
 import { ModalRef } from './shared/components/modal/models/modal-ref';
 import { ModalService } from './shared/components/modal/services/modal.service';
@@ -9,7 +10,7 @@ import { ModalService } from './shared/components/modal/services/modal.service';
   styleUrls: ['./app.component.scss'],
   animations: [fade]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   @ViewChild('modal') public modalTemplateRef!: TemplateRef<any>;
   title = 'acessibilidade-modal';
   public info = false;
@@ -17,12 +18,28 @@ export class AppComponent {
   public modalRef!: ModalRef;
   public firstName = 'Name here';
 
-  constructor(private modalService: ModalService) {}
+  constructor(private modalService: ModalService, private formBuilder: FormBuilder) {}
+  public form!: FormGroup;
 
   public show(): void {
     this.modalRef = this.modalService.open({
       templateRef: this.modalTemplateRef,
       title: 'User Details'
     })
+  }
+
+  public submit(): void {
+    console.log(this.form.value);
+    if(this.form.invalid) return;
+    this.modalRef.close();
+  }
+
+  public ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      firstName: ['Flavio', Validators.required],
+      surname: ['', Validators.required],
+      age: ['', Validators.required],
+      info: [false]
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -14,6 +15,7 @@ import { ModalModule } from './shared/components/modal/modal.module';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     ModalModule
   ],
   providers: [],
